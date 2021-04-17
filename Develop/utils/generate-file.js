fbc9aa369f75afd4f8b25d9ff4e14693c405430c
Christopher Campbell <campbefs@gmail.com>
2021-04-16 23:58:30 -0700

    skeleton

---
+++ Develop/utils/generate-file.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('../dist/README.md', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok:true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
+writeFile('test2');
+
+module.exports = writeFile;
