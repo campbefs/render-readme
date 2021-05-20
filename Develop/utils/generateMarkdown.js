71347d60b6749d9e45f480e86ae1e7742aed965e
Christopher Campbell <campbefs@gmail.com>
2021-05-19 21:23:45 -0700

    more descriptive readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -38,10 +38,9 @@ function generateMarkdown(data) {
 
   ## Questions
   If you have any questions, you can find my GitHub page [here](https://github.com/${data.github}). You may also reach me by [email](mailto:${data.email}) with additional questions.
-  
 
   ## License
-  [${license[data.license][1]}](${license[data.license][3]})
+  This application is covered under the following license: [${license[data.license][1]}](${license[data.license][3]})
 
   ## Live Application Snapshot
   ![alt text](../Develop/assets/images/${data.screenshot} 'Live Application Screenshot')
